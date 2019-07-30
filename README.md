45ee8ca9b02a56091f2c755e8650295544c3a916
simone letizi <s.letizi@gmail.com>
2019-07-30 10:52:23 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # "To defined"
 
-%%put here project description 
+%%put here project description
 
 ## Deployment
 
@@ -15,3 +15,32 @@
 - Andrea Petreti
 - Gianluca Aguzzi
 
+# v-template
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ dist/app.0b11db6af13c467f68df.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate("app",{
+
+/***/ "./src/assets/logo.png":
+/*!*****************************!*\
+  !*** ./src/assets/logo.png ***!
+  \*****************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/url-loader/dist/cjs.js):\\nError: ENOENT: no such file or directory, open '/Users/mone/Desktop/Scanbage/src/assets/logo.png'\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvYXNzZXRzL2xvZ28ucG5nLmpzIiwic291cmNlcyI6W10sIm1hcHBpbmdzIjoiIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/assets/logo.png\n");
+
+/***/ })
+
+})
