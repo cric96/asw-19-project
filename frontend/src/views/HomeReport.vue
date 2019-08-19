b103c28c2ec6877b659e3d786f7c25a8be98078a
Andrea <petretiandrea@gmail.com>
2019-08-19 13:17:30 +0200

    making home-report dashboard

--- frontend/src/views/HomeReport.vue
+++ frontend/src/views/HomeReport.vue
@@ -7,17 +7,22 @@
             <v-icon v-else>add</v-icon>
           </v-btn>
         </template>
-
         <v-btn fab light>
             <v-icon>camera</v-icon>
         </v-btn>
-
         <v-btn fab light>
             <v-icon>edit</v-icon>
         </v-btn>
-
       </v-speed-dial>
       
+      <v-row dense>
+        <v-col v-for="n in 4" :key="n" cols="12" md="4">
+          <v-card >
+            <v-card-text>ciaoo</v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+
     </v-container>
 </template>
 
