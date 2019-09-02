6e49a5e54d14dd1de4a8c2e088524aa2c3c1f043
petretiandrea <petretiandrea@gmail.com>
2019-09-02 17:50:14 +0200

    fix fetch response

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -17,17 +17,17 @@ const store = new Vuex.Store({
     },
     actions: {
         signUp({ commit }, user) {
-            return usersApi.create_user(user).then((user)=>{
+            return usersApi.create_user(user).then(()=>{
                 return this.dispatch('signIn');
-            })
+            }).catch(() => this.dispatch('logout'));
         },
         signInAndUpdate({ commit }, userToUpdate) {
             return this.dispatch('signIn').then((user)=>{
                 userToUpdate._id = user._id;
                 return usersApi.update_user(userToUpdate)
-            }).then(updatedUser=>{
-                commit('setUserProfile', updatedUser);
-                return updatedUser
+            }).then(response => {
+                commit('setUserProfile', response.data);
+                return response.data
             }).catch((err)=>{
                 console.log(err)
                 this.dispatch('logout');
@@ -39,9 +39,9 @@ const store = new Vuex.Store({
                 firebaseUser.getIdToken(true).then(token => {
                     if(token){
                         commit('setToken', token);
-                        usersApi.get_user().then(user=>{
-                            commit('setUserProfile', user);
-                            resolve(user)
+                        usersApi.get_user().then(response => {
+                            commit('setUserProfile', response.data);
+                            resolve(response.data)
                         }).catch((err)=>{
                             reject(err);
                             this.dispatch('logout');
