c55a02e11fb29e7ad0f1b27aa5948df66fed6436
petretiandrea <petretiandrea@gmail.com>
2019-09-05 17:16:52 +0200

    fix some rest problems

--- backend/controllers/buildingsController.js
+++ backend/controllers/buildingsController.js
@@ -4,12 +4,18 @@ var Building = mongoose.model('Building');
 var BinCategory = require("../models/binCategoryModel");
 var Bin = require("../models/binModel");
 
+
+
 //TO CHECK
-function execIfUserIsInBuilding(mongooseId, building, callback) {
-    if(buildings.members.find(user => mongooseId == user._id)) {
+function execIfUserIsInBuilding(mongooseId, building, callback, res) {
+    if(building.members.find(user => JSON.stringify(mongooseId) == JSON.stringify(user._id))) {
         callback()
     } else {
-        utils.sendResponseMessage(res, 403, "You are not member or owner of this building")
+        if(JSON.stringify(building.owner) != JSON.stringify(mongooseId)) {
+            utils.sendResponseMessage(res, 403, "This operation is allowed only to owner.")
+        } else {
+            utils.sendResponseMessage(res, 403, "You are not member of this building")
+        }
     }
 }
 exports.create_buildings = function(req, res) {
@@ -71,41 +77,42 @@ exports.list_buildings = function(req, res) {
 }
 
 exports.read_building = function(req, res) {
-    building_id = req.params.id
+    let building_id = req.params.id
     Building.findById(building_id)
             .exec()
             .then(building => {
                 if(building == null) {
                     utils.sendResponseMessage(res, 404, "Building " + building_id + " not found")
                 } else {
-                    execIfUserIsInBuilding(res.locals.userAuth._id, building, () => utils.sendResponseMessage(res, 200, building))
+                    execIfUserIsInBuilding(res.locals.userAuth._id, building, () => utils.sendResponseMessage(res, 200, building), res)
                 }
             })
             .catch(err => {
+                console.log(err);
                 utils.sendResponseMessage(res, 500, "Internal error")
             })
 }
 
 exports.update_building = function(req, res) {
-    buildings_id = req.params.id
+    let building_id = req.params.id
+    let mongoose_id = res.locals.userAuth._id
     let updatedBuilding = Building.prepareUpdate(req.body);
     Building.findByIdAndUpdate(building_id, updatedBuilding)
+        .populate('members')    
         .exec()
-        .populate(members)
         .then(buildingReturned => {
             if(buildingReturned == null) {
-                utils.sendResponseMessage(buildingReturned, 404, "Building not found")
+                utils.sendResponseMessage(res, 404, "Building not found")
             } else {
-                execIfUserIsInBuilding(res.locals.uid, buildings, () => utils.sendResponseMessage(res, 200, buildingReturned))
+                execIfUserIsInBuilding(mongoose_id, buildingReturned, () => utils.sendResponseMessage(res, 200, buildingReturned), res)
             }
-            utils.sendResponseMessage(res, 200, res)
         }).catch(err => utils.sendResponseMessage(res, 500, "Internal error"))
 }
 
 exports.add_building_member = function(req, res) {
-    building_id = req.params.id
-    mongoose_id = res.locals.userAuth._id
-    Building.findById(buildings_id).exec()
+    let building_id = req.params.id
+    let mongoose_id = res.locals.userAuth._id
+    Building.findById(building_id).exec()
         .then(building => {
 
         })
--- backend/models/buildingModel.js
+++ backend/models/buildingModel.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var utils = require("../utils/utils");
 var Schema = mongoose.Schema;
 var BinSchema = require('./binModel').schema;
 
