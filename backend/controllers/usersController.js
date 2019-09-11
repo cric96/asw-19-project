4399ecc5026f811f90f54a76885f26096c9d8354
gianluca.aguzzi <gianluca.aguzzi@studio.unibo.it>
2019-09-11 18:07:42 +0200

    Refactor userController

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var utils = require("../utils/utils");
 var User = mongoose.model('User');
 
-
+//IMPROVE
 exports.create_user = function(req, res) {
     var user = new User(req.body);
     console.log(user)
@@ -11,44 +11,40 @@ exports.create_user = function(req, res) {
         console.log(error)
         res.setBadRequest("Bad request; email and firebase_uid are required fields");
     } else {
-        user.save(function(err, newUser) {
-            if (!err && newUser) {
-                res.setCreated(newUser);
-            } else {
-                console.log(err)
-                if (err.code == 11000) {
+        user.save()
+            .exec()
+            .then(newUser => res.setCreated(newUser))
+            .catch(err => {
+                //check if put this code inside decore
+                if(err.code == 11000) { //TODO PUT COSTANT
                     res.setConflict("Conflict: " + err.errmsg);
                 } else {
                     res.setInternalError();
                 }
-            }
-        });
+            })
     }
 };
 
 exports.get_user = function(req, res) {
     let uid = res.locals.uid;
-    User.findOne({ firebase_uid: uid })
-        .exec()
-        .then(user => res.setOkIfNotNull(user, "User not found"))
-        .catch(err => res.setInternalError(err))
+    if(res.locals.userAuth._id != req.params.id) {
+        res.setForbidden("The id must match with the logged user")
+    } else {
+        User.findOne({ firebase_uid: uid })
+            .exec()
+            .then(user => res.setOkIfNotNull(user, "User not found"))
+            .catch(err => res.setInternalError(err))
+    }
+    
 }
 
 exports.update_user = function(req, res) {
     let uid = res.locals.uid;
     let updateUser = User.prepareUpdate(req.body);
-    User.findOneAndUpdate({ firebase_uid: uid }, updateUser, { new: true }, function(err, updatedUser) {
-        if (err) {
-            console.log(err)
-            res.setBadRequest();
-        } else {
-            if (updatedUser == null) {
-                res.setNotFound("User not found")
-            } else {
-                res.setOk(updateUser)
-            }
-        }
-    });
+    User.findOneAndUpdate({ firebase_uid: uid }, updateUser, { new: true })
+        .exec()
+        .then(updatedUser => res.setOkIfNotNull(updatedUser, "User not found"))
+        .catch(err => res.setBadRequest())
 };
 
 exports.get_all_users = function(req, res) {
