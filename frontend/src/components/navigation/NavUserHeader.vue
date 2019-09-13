26ea3e4a60f8f55b9c300459f6d4a2810050bd70
petretiandrea <petretiandrea@gmail.com>
2019-09-13 15:45:42 +0200

    Update NavUserHeader.vue

--- frontend/src/components/navigation/NavUserHeader.vue
+++ frontend/src/components/navigation/NavUserHeader.vue
@@ -4,6 +4,11 @@
             <v-list-item-avatar color="secondary" class="headline font-weight-light white--text">
                {{user.level}}
             </v-list-item-avatar>
+            <div class="my-2" justify-end>
+                <v-btn color="primary" rounded icon outlined x-small @click="$emit('clickUser')">
+                    <v-icon>mdi-pencil</v-icon>
+                </v-btn>
+            </div>
         </v-list-item>
         <v-list-item>
             <v-list-item-content>
