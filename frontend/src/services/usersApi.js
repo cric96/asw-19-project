92f3acb44ba1cdb0cc826111c7a2cb26d2742404
Andrea <petretiandrea@gmail.com>
2019-09-22 11:41:37 +0200

    filter and limit frontend members

--- frontend/src/services/usersApi.js
+++ frontend/src/services/usersApi.js
@@ -7,8 +7,8 @@ export default {
     getUser : function(firebaseUid) {
         return apiService.get(`/users/${firebaseUid}`, null, true)
     },
-    getAllUsers: function(filter = null) {
-        return apiService.get(`/users/`, { 'filter': filter }, true)
+    getAllUsers: function(filter = null, limit = 5) {
+        return apiService.get(`/users/`, { filter: filter, limit: limit }, true)
     },
     updateUser : function(user) {
         return apiService.put(`/users/${user.firebase_uid}`, user, null, true)
