a17449aadb147560b7e74a320ad3cb7a0ae30de8
gianluca.aguzzi <gianluca.aguzzi@studio.unibo.it>
2019-10-13 10:29:50 +0200

    work on leaderboard

--- frontend/src/views/Leaderboard.vue
+++ frontend/src/views/Leaderboard.vue
@@ -8,53 +8,173 @@
                 <v-tab>Per Livello</v-tab>
                 <v-tab>Per Punteggio</v-tab>
                 <v-tab>Per Rifiuto</v-tab>
-
-
-                
+               
                 <v-tab-item
                     v-for="n in 3"
                     :key="n"
                 >
                     <v-container fluid>
+                        <v-layout row justify-center>
+                            <v-flex xs12 md2 v-if="$vuetify.breakpoint.smAndDown">
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-icon>fa-trash</v-icon>                              
+                                        Filtri
+                                    </v-flex>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs12 md2>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-icon>fa-trash</v-icon>                              
+                                        Categoria
+                                    </v-flex>
+                                    <template v-else>
+                                        <v-icon>fa-trash</v-icon>                              
+                                        Categoria
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs12 md2>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                    <v-icon>compare_arrows</v-icon>
+                                            Ordinamento
+                                    </v-flex>
+                                    <template v-else>
+                                    <v-icon>compare_arrows</v-icon>
+                                            Ordinamento
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs12 md2>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex  offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                    <v-icon>location_city</v-icon>
+                                            Città
+                                    </v-flex>
+                                    <template v-else>
+                                    <v-icon>location_city</v-icon>
+                                            Città
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs12 md2>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                    <v-icon>face</v-icon>
+                                            Soggetto
+                                    </v-flex>
+                                    <template v-else>
+                                    <v-icon>face</v-icon>
+                                            Soggetto
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs4 md1 v-if="$vuetify.breakpoint.smAndDown">
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                         Annulla
+                                    </v-flex>
+                                    <template v-else>                           
+                                        Annulla
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs4 md1>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                        Reset
+                                    </v-flex>
+                                    <template v-else>
+                                        Reset
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                            <v-flex xs4 md1>
+                                <v-btn block class="text-left" text height="60">
+                                    <v-flex offset-xs2 v-if="$vuetify.breakpoint.smAndDown">
+                                        Applica
+                                    </v-flex>
+                                    <template v-else>
+                                       Applica
+                                    </template>
+                                </v-btn>
+                            </v-flex>
+                        </v-layout>
+                        
+<!--
+                        <v-row no-gutters>
+                            
+                            <v-col>
+                                <v-btn block class="text-left" text height="60">
+                                    <template v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-flex>
+                                            <v-icon>fa-trash</v-icon>                              
+                                            Categoria
+                                        </v-flex>
+                                    </template>
+                                    <template v-else>
+                                        <v-icon>fa-trash</v-icon>                              
+                                        Categoria
+                                    </template>
+                                </v-btn>
+                            </v-col>
+                            <v-col> 
+                                <v-btn block class="text-left" text height="60">
+                                    <template v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-flex>
+                                            <v-icon>compare_arrows</v-icon>
+                                            Ordinamento
+                                        </v-flex>
+                                    </template>
+                                    <template v-else>
+                                        <v-icon>compare_arrows</v-icon>
+                                        Ordinamento
+                                    </template>
+                                </v-btn>
+                            </v-col>
+                            <v-col>
+                                <v-btn block class="text-left" text height="60">
+                                    <template v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-flex>
+                                            <v-icon>location_city</v-icon>
+                                            Città
+                                        </v-flex>
+                                    </template>
+                                    <template v-else>
+                                        <v-icon>location_city</v-icon>
+                                            Città
+                                    </template>
+                                </v-btn>
+                            </v-col>
+                            <v-col>
+                                <v-btn block class="text-left" text height="60">
+                                    
+                                    <template v-if="$vuetify.breakpoint.smAndDown">
+                                        <v-flex>
+                                            <v-icon>face</v-icon>
+                                            Soggetto
+                                        </v-flex>
+                                    </template>
+                                    <template v-else>
+                                        <v-icon>face</v-icon>
+                                        Soggetto
+                                    </template>
+                                </v-btn>
+                            </v-col>
 
-                         <div> 
-      <v-toolbar flat> <!--TODO make collapse working-->
-       
-        <v-toolbar-title>
-            Filtri 
-            <v-icon>arrow_forward_ios</v-icon>
-        </v-toolbar-title>
-  
-            <v-toolbar-items>
-                <v-btn text>
-                    <v-icon>fa-trash</v-icon>
-                    Categoria
-                </v-btn>
-                <v-btn text>
-                    <v-icon>compare_arrows</v-icon>
-                    Ordinamento
-                </v-btn>
-                <v-btn text>
-                    <v-icon>location_city</v-icon>
-                    Città
-                </v-btn>
-                <v-btn text>
-                    <v-icon>face</v-icon>
-                    Soggetto
-                </v-btn>
+                        </v-row>  
+                        -->   
+               <!-- 
                 <v-overflow-btn
                     class="my-2"
                     :items="['ciao', 'stronzo']"
                     label="Overflow Btn"
-        ></v-overflow-btn>
-         
-        </v-toolbar-items>
+                ></v-overflow-btn>
+                 <v-menu top :close-on-click="closeOnClick">
+      <template v-slot:activator="{ on }">-->
 
-        
-  
-    
-      </v-toolbar>
-    </div>
                     </v-container>
                 </v-tab-item>
             </v-tabs>
